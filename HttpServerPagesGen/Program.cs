f687b2bbc282bc308772378693d5d69e8ebee88a
Nikita <cc.tapa@gmail.com>
2015-11-23 15:10:54 +0300

    generate fsdata.c file for "fs" directory content (without any startup configure flags)

--- HttpServerPagesGen/Program.cs
+++ HttpServerPagesGen/Program.cs
@@ -13,6 +13,26 @@ namespace HttpServerPagesGen
         {
             private readonly string _file;
 
+            private static Dictionary<string, string> _supportedExtHTTPContentType = new Dictionary<string, string> {
+             { "html",  "text/html"},
+             { "htm",   "text/html" },
+             { "shtml", "text/html\r\nExpires: Fri, 10 Apr 2008 14:00:00 GMT\r\nPragma: no-cache"},
+             { "shtm",  "text/html\r\nExpires: Fri, 10 Apr 2008 14:00:00 GMT\r\nPragma: no-cache"},
+             { "ssi",   "text/html\r\nExpires: Fri, 10 Apr 2008 14:00:00 GMT\r\nPragma: no-cache"},
+             { "gif",   "image/gif"},
+             { "png",   "image/png"},
+             { "jpg",   "image/jpeg"},
+             { "bmp",   "image/bmp"},
+             { "ico",   "image/x-icon"},
+             { "class", "application/octet-stream"},
+             { "cls",   "application/octet-stream"},
+             { "js",    "application/x-javascript"},
+             { "ram",   "application/x-javascript"},
+             { "css",   "text/css"},
+             { "swf",   "application/x-shockwave-flash"},
+             { "xml",   "text/xml"}
+            };
+
             public FileDescriptor(string file)
             {
                 _file = file;
@@ -26,7 +46,16 @@ namespace HttpServerPagesGen
                 }
             }
 
-            public string FormatedFileName
+            public string ServerFileName
+            {
+                get
+                {
+                    return _file.Replace(DEFAULT_DIR, String.Empty)
+                                .Replace("\\", "/");
+                }
+            }
+
+            public string FileNameInCFormat
             {
                 get
                 {
@@ -35,13 +64,109 @@ namespace HttpServerPagesGen
                                 .Replace("\\", "_");
                 }
             }
+
+            public string FileExtension
+            {
+                get
+                {
+                    return Path.GetExtension(_file)
+                               .Replace(".", String.Empty);
+                }
+            }
+
+            public string ServerFileNameInHEX
+            {
+                get
+                {
+                    var result = new StringBuilder();
+                    ASCIIResult fileName = ToHEXFormat($"{ServerFileName}\0");
+
+                    result.Append($"/* {ServerFileName} ({fileName.Size} bytes) */ {newline}");
+                    result.Append(fileName.ASCIIDataWithComma);
+
+                    return result.ToString();
+                }
+            }
+
+            public int ServerFileNameLen
+            {
+                get
+                {
+                    return ToHEXFormat($"{ServerFileName}\0").Size;
+                }
+            }
+
+            public string HTTPContentType
+            {
+                get
+                {
+                    var result = new StringBuilder();
+
+                    var contentType = GetHTTPContentType(FileExtension);
+                    ASCIIResult contentTypeFull = ToHEXFormat($"Content-type: {contentType}{newline}{newline}");
+
+                    result.Append($"/* Content-type: {contentType} ({contentTypeFull.Size} bytes)*/{newline}");
+                    result.Append(contentTypeFull.ASCIIDataWithComma);
+
+                    return result.ToString();
+                }
+            }
+
+            private static string GetHTTPContentType(string ext)
+            {
+                string contentType;
+                if (_supportedExtHTTPContentType.TryGetValue(ext, out contentType))
+                    return contentType;
+
+                return DEFAULT_HTTP_CONTENT_TYPE;
+            }
+
+            public string HTTPData
+            {
+                get
+                {
+                    var result = new StringBuilder();
+                    var fileData = File.ReadAllBytes(FileName);
+                    ASCIIResult data = ToHEXFormat(fileData);
+                    result.Append($"/* RAW Data ({data.Size} bytes)*/ {newline}");
+                    result.Append(data.ASCIIData);
+
+                    return result.ToString();
+                }
+            }
         }
+
+        class ASCIIResult
+        {
+            public ASCIIResult(string data, int size)
+            {
+                Size = size;
+                ASCIIData = data;
+            }
+
+            public string ASCIIData { get; private set; }
+            public int Size { get; private set; }
+
+            public string ASCIIDataWithComma
+            {
+                get
+                {
+                    return !String.IsNullOrEmpty(ASCIIData) ? ASCIIData + "," : ASCIIData;
+                }
+            }
+        }
+
+        private static string newline = Environment.NewLine;
+
+        private static string DEFAULT_HTTP_CONTENT_TYPE = "text/plain";
 
         private const string DEFAULT_DIR = "fs";
 
         private const int BYTES_PER_LINE = 16;
 
-        private string newline = Environment.NewLine;
+        private static string SERVER_FOOT_PRINT = "lwIP/1.3.2";
+
+        private static string LastModifiedDate = DateTime.Now.ToString("r");
 
         public FSDataCreator()
         {
@@ -52,19 +177,22 @@ namespace HttpServerPagesGen
         {
             var result = new StringBuilder();
 
-            result.Append(GenerateHeader());
+            /* C header */
+            result.Append(GenerateCHeader());
 
-            var files = new List<FileDescriptor>();
+            var files = new LinkedList<FileDescriptor>();
             GetFiles(DEFAULT_DIR, files);
 
+            /* Data section */
             result.Append(GenerateData(files));
 
-            var res = result.ToString();
+            /* Struct section */
+            result.Append(GenerateFiles(files));
 
-            return res;
+            return result.ToString();
         }
 
-        private string GenerateHeader()
+        private string GenerateCHeader()
         {
             var result = new StringBuilder();
 
@@ -74,7 +202,7 @@ namespace HttpServerPagesGen
 
             result.Append(newline);
 
-            result.Append("#define file_NULL (struct fsdata_file *) NULL");
+            result.Append($"#define file_NULL (struct fsdata_file *) NULL {newline}");
 
             result.Append(newline);
 
@@ -86,64 +214,150 @@ namespace HttpServerPagesGen
         private string GenerateData(IEnumerable<FileDescriptor> files)
         {
             var result = new StringBuilder();
+            int i = 0;
             foreach (var file in files)
             {
-                var fileData = File.ReadAllText(file.FileName);
-
-                result.Append($"static const unsigned char data_{file.FormatedFileName}[] = {{ {newline}");
+                result.Append($"static const unsigned int dummy_align__{file.FileNameInCFormat} = {i};{newline}");
+                result.Append($"static const unsigned char data__{file.FileNameInCFormat}[] = {{ {newline}");
 
                 /* File name */
-                result.Append($"/* {file.FileName} */ {newline}");
-                result.Append(ToHEXFormat(file.FileName));
-
+                result.Append(file.ServerFileNameInHEX);
                 result.Append(newline);
 
                 /* HTTP Header */
-                result.Append($"/* HTTP header */ {newline}");
-                result.Append(ToHEXFormat($"HTTP/1.0 200 OK"));
+                result.Append(GetHTTPHeader());
+                result.Append(newline);
 
+                /* Last-Modified */
+                result.Append(GetLastModified());
                 result.Append(newline);
 
                 /* Server */
-                result.Append($"/* Server */ {newline}");
-                result.Append(ToHEXFormat($"Server: lwIP/1.3.1"));
-
+                result.Append(GetServerFootPrint());
                 result.Append(newline);
 
                 /* Content-type */
-                result.Append($"/* Content-type */ {newline}");
-                result.Append(ToHEXFormat($"Content-type: text/html"));
+                result.Append(file.HTTPContentType);
+                result.Append(newline);
+                result.Append(newline);
 
-                result.Append(newline);
-                result.Append(newline);
 
                 /* Data */
-                result.Append(ToHEXFormat(fileData));
+                result.Append(file.HTTPData);
 
                 result.Append("};");
 
                 result.Append(newline);
                 result.Append(newline);
 
-                Console.WriteLine($"file data '{file.FormatedFileName}' generated");
+                Console.WriteLine($"file data '{file.FileNameInCFormat}' generated");
+
+                i++;
             }
 
             return result.ToString();
         }
 
-
-        private static void GetFiles(string folderName, List<FileDescriptor> files)
+        private static string GetHTTPHeader()
         {
-            foreach (var file in Directory.GetFiles(folderName))
-                files.Add(new FileDescriptor(file));
+            var result = new StringBuilder();
+            ASCIIResult header = ToHEXFormat($"HTTP/1.0 200 OK{newline}");
+            result.Append($"/* HTTP/1.0 200 OK ({header.Size} bytes)*/{newline}");
+            result.Append(header.ASCIIDataWithComma);
 
-            foreach (var dir in Directory.GetDirectories(folderName))
-                GetFiles(dir, files);
+            return result.ToString();
         }
 
-        private static string ToHEXFormat(string data)
+        private static string GetLastModified()
         {
-            return String.Join(",", Encoding.ASCII.GetBytes(data).Select((n, i) =>
+            var result = new StringBuilder();
+            ASCIIResult lastModified = ToHEXFormat($"Last-Modified: {LastModifiedDate}{newline}");
+            result.Append($"/* Last-Modified: {LastModifiedDate} ({lastModified.Size} bytes) */{newline}");
+            result.Append(lastModified.ASCIIDataWithComma);
+
+            return result.ToString();
+        }
+
+        private static string GetServerFootPrint()
+        {
+            var result = new StringBuilder();
+            ASCIIResult server = ToHEXFormat($"Server: {SERVER_FOOT_PRINT}{newline}");
+            result.Append($"/* Server: {SERVER_FOOT_PRINT} ({server.Size} bytes) */{newline}");
+            result.Append(server.ASCIIDataWithComma);
+
+            return result.ToString();
+        }
+
+        private string GenerateFiles(LinkedList<FileDescriptor> files)
+        {
+            var result = new StringBuilder();
+
+            /*
+
+            struct fsdata_file {
+                const struct fsdata_file *next;
+                const unsigned char *name;
+                const unsigned char *data;
+                int len;
+                u8_t http_header_included;
+             };
+
+            */
+
+            LinkedListNode<FileDescriptor> node = files.First;
+
+            while (node != null)
+            {
+                bool isFirst = node.Previous == null;
+                FileDescriptor file = node.Value;
+
+                result.Append($"const struct fsdata_file file__{file.FileNameInCFormat}[] = {{{{{newline}");
+                if (isFirst)
+                    result.Append($"\tfile_NULL,{newline}");
+                else
+                    result.Append($"\tfile__{node.Previous.Value.FileNameInCFormat},{newline}");
+
+                result.Append($"\tdata__{file.FileNameInCFormat},{newline}");
+                result.Append($"\tdata__{file.FileNameInCFormat} + {file.ServerFileNameLen},{newline}");
+                result.Append($"\tsizeof(data__{file.FileNameInCFormat}) - {file.ServerFileNameLen},{newline}");
+                result.Append($"\t1{newline}");
+                result.Append($"}}}};{newline}");
+
+                result.Append(newline);
+
+                node = node.Next;
+            }
+
+            result.Append(newline);
+
+            result.Append($"#define FS_ROOT file__{files.Last.Value.FileNameInCFormat}{newline}");
+            result.Append($"#define FS_NUMFILES {files.Count}");
+
+            result.Append(newline);
+
+            return result.ToString();
+        }
+
+
+        private static void GetFiles(string folderName, LinkedList<FileDescriptor> files)
+        {
+            foreach (var dir in Directory.GetDirectories(folderName))
+                GetFiles(dir, files);
+
+            foreach (var file in Directory.GetFiles(folderName))
+                files.AddLast(new FileDescriptor(file));
+        }
+
+        private static ASCIIResult ToHEXFormat(string data)
+        {
+            var bytes = Encoding.ASCII.GetBytes(data);
+
+            return ToHEXFormat(bytes);
+        }
+
+        private static ASCIIResult ToHEXFormat(byte[] data)
+        {
+            var asciiData = String.Join(",", data.Select((n, i) =>
             {
                 var res = $"0x{n:x2}";
 
@@ -152,6 +366,8 @@ namespace HttpServerPagesGen
 
                 return res;
             }));
+
+            return new ASCIIResult(asciiData, data.Count());
         }
     }
 
@@ -160,7 +376,9 @@ namespace HttpServerPagesGen
         static void Main(string[] args)
         {
             var creator = new FSDataCreator();
-            creator.Create();
+            var result = creator.Create();
+
+            File.WriteAllText("fsdata.c", result);
         }
     }
 }
